bd85b1b55d3d5761808cf9aebf812063cf8d4e41
Thibault Kummer <thibault.kummer@yellowkorner.fr>
2024-10-13 18:48:46 +0200

    Energy details

--- htdocs/js/src/meter/Global.js
+++ htdocs/js/src/meter/Global.js
@@ -35,12 +35,29 @@ export class Global extends React.Component
 		return f.toFixed(1) + unit;
 	}
 
+	formatEnergy(f) {
+		if(f===undefined || f==='')
+			return '';
+
+		f = parseFloat(f);
+
+		let unit = 'Wh';
+		let digits = 0;
+		if(f>=1000)
+		{
+			f = f / 1000;
+			unit = 'kWh';
+			digits = 1;
+		}
+
+		return f.toFixed(digits) + unit;
+	}
+
 	update(data) {
 		this.setState(data);
 	}
 
 	render() {
-		console.log(this.state);
 		return (
 			<div className="sc-meter-global">
 				<div className="item">
@@ -66,6 +83,23 @@ export class Global extends React.Component
 				<div className="item">
 					<div className="value">{this.formatPower(this.state.excess)}</div>
 					<div className="label">Excess</div>
+				</div>
+
+				<div className="item">
+					<div className="value">{this.formatEnergy(this.state.grid_energy)}</div>
+					<div className="label">Imported</div>
+				</div>
+				<div className="item">
+					<div className="value">{this.formatEnergy(this.state.grid_exported_energy)}</div>
+					<div className="label">Exported</div>
+				</div>
+				<div className="item">
+					<div className="value">{this.formatEnergy(this.state.pv_energy)}</div>
+					<div className="label">Produced</div>
+				</div>
+				<div className="item">
+					<div className="value">{this.formatEnergy(this.state.hws_energy)}</div>
+					<div className="label">HWS</div>
 				</div>
 			</div>
 		);
