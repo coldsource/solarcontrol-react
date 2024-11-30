ad6df098051b3cf61bdd030ed65c501ab3dff3ae
Thibault Kummer <thibault.kummer@yellowkorner.fr>
2024-11-30 16:08:42 +0100

    Today summary

--- htdocs/js/src/logs/EnergyDetail.js
+++ htdocs/js/src/logs/EnergyDetail.js
@@ -24,6 +24,52 @@ export class EnergyDetail extends React.Component
 		});
 	}
 
+	renderDailySummary() {
+		let summary = {};
+
+		let today = new Date();
+		today.setHours(0,0,0,0);
+
+		for(const [date, devices] of Object.entries(this.state.energy))
+		{
+			let d = new Date(date);
+			if(d.getTime()<today.getTime())
+				continue;
+
+			for(const [device_id, data] of Object.entries(devices))
+			{
+				if(device_id==0)
+				{
+					if(summary[device_id]===undefined)
+						summary[device_id] = {grid: 0, hws: 0, pv: 0};
+
+					summary[device_id].grid += data.grid;
+					summary[device_id].hws += data.hws;
+					summary[device_id].pv += data.pv;
+				}
+				else
+				{
+					if(summary[device_id]===undefined)
+						summary[device_id] = {name: data.name, device: 0};
+
+					summary[device_id].device += data.device;
+				}
+			}
+		}
+
+		return (
+			<div>
+				<div className="date">Today</div>
+				<table>
+					<tbody>
+						{this.renderDevices(summary, true)}
+						{this.renderSummary(summary, true)}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+
 	renderDays() {
 		return Object.keys(this.state.energy).sort().reverse().map(date => {
 			const devices = this.state.energy[date];
@@ -41,7 +87,7 @@ export class EnergyDetail extends React.Component
 		});
 	}
 
-	renderDevices(devices) {
+	renderDevices(devices, nopower = false) {
 		return Object.keys(devices).map(device_id => {
 			if(device_id==0)
 				return;
@@ -51,14 +97,14 @@ export class EnergyDetail extends React.Component
 			return (
 				<tr key={device_id}>
 					<td>{data.name}</td>
-					<td><KW value={data.device * 4} /></td>
+					<td>{nopower?'':(<KW value={data.device * 4} />)}</td>
 					<td><KWh value={data.device} /></td>
 				</tr>
 			);
 		});
 	}
 
-	renderSummary(devices) {
+	renderSummary(devices, nopower = false) {
 		if(devices["0"]===undefined)
 			return;
 
@@ -68,17 +114,17 @@ export class EnergyDetail extends React.Component
 			<React.Fragment>
 				<tr>
 					<td>Grid</td>
-					<td><KW value={data.grid * 4} /></td>
+					<td>{nopower?'':(<KW value={data.grid * 4} />)}</td>
 					<td><KWh value={data.grid} /></td>
 				</tr>
 				<tr>
 					<td>PV</td>
-					<td><KW value={data.pv * 4} /></td>
+					<td>{nopower?'':(<KW value={data.pv * 4} />)}</td>
 					<td><KWh value={data.pv} /></td>
 				</tr>
 				<tr>
 					<td>HWS</td>
-					<td><KW value={data.hws * 4} /></td>
+					<td>{nopower?'':(<KW value={data.hws * 4} />)}</td>
 					<td><KWh value={data.hws} /></td>
 				</tr>
 			</React.Fragment>
@@ -88,6 +134,7 @@ export class EnergyDetail extends React.Component
 	render() {
 		return (
 			<div className="sc-logsenergydetail">
+				{this.renderDailySummary()}
 				{this.renderDays()}
 			</div>
 		);
