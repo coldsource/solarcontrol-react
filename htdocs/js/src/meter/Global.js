f5c07511c43236a39e74c6b8a80f6853be89c544
Thibault Kummer <thibault.kummer@yellowkorner.fr>
2024-12-29 17:44:01 +0100

    New home screen

--- htdocs/js/src/meter/Global.js
+++ htdocs/js/src/meter/Global.js
@@ -10,6 +10,8 @@ export class Global extends React.Component
 		this.state = {
 		}
 
+		this.ref = React.createRef();
+
 		this.update = this.update.bind(this);
 	}
 
@@ -32,48 +34,82 @@ export class Global extends React.Component
 		return (<i className="scf scf-leaf" />);
 	}
 
+	calcPowerMixStyle() {
+		let prct = 100;
+		if(this.state.grid>0)
+			prct = this.state.pv / (this.state.pv + this.state.grid) * 100;
+		return {background: `linear-gradient(0deg, rgba(51,201,85,1) 0%, rgba(51,201,85,1) ${prct}%, rgba(255,255,255,1) ${prct + 5}%, rgba(255,255,255,1) 100%)`};
+	}
+
+	getPVRatio() {
+		let pv_consumed = this.state.pv_energy - this.state.grid_exported_energy;
+		return pv_consumed / (this.state.grid_energy + pv_consumed) * 100;
+	}
+
+	calcEnergyMixStyle() {
+		let prct = this.getPVRatio();
+		return {background: `linear-gradient(90deg, rgba(51,201,85,1) 0%, rgba(51,201,85,1) ${prct}%, rgba(255,255,255,1) ${prct + 5}%, rgba(255,255,255,1) 100%)`};
+	}
+
 	render() {
 		return (
 			<div className="sc-meter-global">
-				<div className="item">
-					<div className="value"><KW value={this.state.grid} /></div>
-					<div className="label">Grid power {this.renderLeaf()}</div>
+				<div className="production">
+					<div className="round">
+						<div><i className="scf scf-electricity" /></div>
+						<span className="meter right"><KWh value={this.state.grid_energy} /></span>
+					</div>
+					<div></div>
+					<div className="round">
+						<div><i className="scf scf-sun" /></div>
+						<span className="meter left"><KWh value={this.state.pv_energy} /></span>
+					</div>
 				</div>
-				<div className="item">
-					<div className="value"><KWh value={this.state.grid_energy} /></div>
-					<div className="label">Imported</div>
+				<div className="network">
+					<div className="connector-grid">
+						{this.state.grid<0?(<img className="arrow-grid" src="/images/arrow-up.svg" />):''}
+						{this.state.grid>=0?(<img className="arrow-bolt" src="/images/arrow-right.svg" />):''}
+						<span className="meter"><KW value={this.state.grid} /></span>
+					</div>
+					<div className="round bolt" style={this.calcPowerMixStyle()}>
+						<div><i className="scf scf-bolt" /></div>
+					</div>
+					<div className="connector-pv">
+						<img className="arrow-bolt" src="/images/arrow-left.svg" />
+						<span className="meter"><KW value={this.state.pv} /></span>
+					</div>
 				</div>
-				<div className="item">
-					<div className="value"><KW value={this.state.pv} /></div>
-					<div className="label">PV power</div>
+				<div className="network-vert">
+					<img className="arrow-down" src="/images/arrow-down.svg" />
+					<span className="meter"><KW value={this.state.total} /></span>
 				</div>
-				<div className="item">
-					<div className="value"><KWh value={this.state.pv_energy} /></div>
-					<div className="label">PV production</div>
+				<div className="home" style={this.calcEnergyMixStyle()}>
+					<span><KW value={this.state.grid_energy + this.state.pv_energy - this.state.grid_exported_energy} /></span>
+					<i className="scf scf-house" />
+					<span>{this.getPVRatio().toFixed(0) + '% PV'}</span>
 				</div>
-				<div className="item">
-					<div className="value"><KW value={this.state.hws} /></div>
-					<div className="label">HWS power</div>
-				</div>
-				<div className="item">
-					<div className="value"><KWh value={this.state.hws_energy} /></div>
-					<div className="label">HWS energy</div>
-				</div>
-				<div className="item">
-					<div className="value"><KW value={this.state.net_available} /></div>
-					<div className="label">Net available</div>
-				</div>
-				<div className="item">
-					<div className="value"><KW value={this.state.gross_available} /></div>
-					<div className="label">Gross available</div>
-				</div>
-				<div className="item">
-					<div className="value"><KW value={this.state.total} /></div>
-					<div className="label">Total consumption</div>
-				</div>
-				<div className="item">
-					<div className="value"><KWh value={this.state.grid_exported_energy} /></div>
-					<div className="label">Exported</div>
+				<div className="home-devices">
+					<div></div>
+					<div>
+						<div className="network-vert">
+							<img className="arrow-down" src="/images/arrow-down.svg" />
+							<span className="meter"><KW value={this.state.hws} /></span>
+						</div>
+						<div className="round">
+							<div><i className="scf scf-droplet" /></div>
+							<span className="meter bottom"><KWh value={this.state.hws_energy} /></span>
+						</div>
+					</div>
+					<div>
+						<div className="network-vert">
+							<img className="arrow-down" src="/images/arrow-down.svg" />
+							<span className="meter"><KW value={this.state.net_available} /></span>
+						</div>
+						<div className="round">
+							<div><i className="scf scf-available-energy" /></div>
+						</div>
+					</div>
+					<div></div>
 				</div>
 			</div>
 		);
