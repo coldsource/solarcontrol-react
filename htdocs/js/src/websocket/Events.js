20f0bfb7c8fe3c90a9ecf254a5a38ff0782b2d06
Thibault Kummer <thibault.kummer@yellowkorner.fr>
2025-09-11 17:22:32 +0200

    Auto reconnection

--- htdocs/js/src/websocket/Events.js
+++ htdocs/js/src/websocket/Events.js
@@ -3,6 +3,9 @@ export class Events
 	constructor(protocol)
 	{
 		this.protocol = protocol;
+
+		this.connected = false;
+		this.reconnect_interval = null;
 	}
 
 	async connect()
@@ -13,12 +16,24 @@ export class Events
 
 		// Event on connection
 		self.ws.onopen = function (event) {
+			self.connected = true;
 		};
 
 		// Event on disconnection
 		self.ws.onclose = function(ev) {
-			if(ev.reason)
-				console.log("Disconnected : " + ev.reason);
+			console.error("Disconnected" + (ev.reason?' : « ' + ev.reason + ' »':''));
+
+			self.connected = false;
+			self.reconnect_interval = setInterval(() => {
+				if(self.connected)
+				{
+					clearInterval(self.reconnect_interval);
+					return;
+				}
+
+				console.log("Reconnecting...");
+				self.connect();
+			}, 10000);
 		}
 
 		self.ws.onmessage = function (ev) {
